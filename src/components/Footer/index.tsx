3858ef62e3e2737a102623a3cb2834619bab46fd
Ramsey Louder <RamseyLouder@gmail.com>
2024-09-12 15:07:46 -0400

    working on footer css

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import Bootstrap JS and Popper.js
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-import './index.css'
+import "./index.css";
 
-
-export default function Footer(){
-    return(
-        <>
-        <div className="footer-div" ><h3 className="footer-h1">Find me at: </h3><a className="nav-link" href="https://www.linkedin.com/in/ramseyjlouder/">LinkedIn</a><a href="https://github.com/L-Code23/"></a>
+export default function Footer() {
+  return (
+    <>
+      <div className="footer-div">
         
+          <h3 className="footer-h1">Find me at: </h3>
+          <a
+            className="nav-link footer-links"
+            href="https://www.linkedin.com/in/ramseyjlouder/"
+          >
+            LinkedIn
+          </a>
+          <a
+            className="nav-link footer-links"
+            href="https://github.com/L-Code23/"
+          >
+            GitHub
+          </a>
+        
+
+      </div>
         <small>Louder Development © 2024</small>
-        </div>
-        
-        
-        </>
-    )
+    </>
+  );
 }
